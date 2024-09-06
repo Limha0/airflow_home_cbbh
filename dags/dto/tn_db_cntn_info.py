2efcfbe83ad45c50cea2021822f234047a6243c9
Limha0 <limhayoung@socsoft.co.kr>
2024-09-06 17:13:07 +0900

    tn_db_cntn_info 추가

---
+++ dags/dto/tn_db_cntn_info.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+
+Base = declarative_base()
+
+class TnDBCntnInfo(Base):
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    __tablename__ = "tn_db_cntn_info"
+
+    link_db_id = Column(String, primary_key=True)
+    pvdr_sys_nm = Column(String)
+    db_knd_cd = Column(String)
+    sid = Column(String)
+    ip = Column(String)
+    port = Column(String)
+    usid = Column(String)
+    pswd = Column(String)
+    driver_class_nm = Column(String)
